defd90276f7d10564f4be1b65459f7f77f687528
lightgleb <prorocker7@gmail.com>
2023-02-27 16:52:32 +0300

    Добавление тестов для контактов. Создание вспомогательного класса. Пока нерабочий тест по созданию контакта с пустыми значениями полей.

---
+++ contact.py
@@ -0,0 +1,38 @@
+
+class Contact:
+
+    def __init__(self,
+                 firstname,
+                 middlename,
+                 lastname,
+                 nickname,
+                 photo,
+                 title,
+                 company,
+                 address,
+                 mobile,
+                 email,
+                 homepage,
+                 bday,
+                 bmonth,
+                 byear,
+                 address2,
+                 phone2,
+                 notes):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.photo = photo
+        self.title = title
+        self.company = company
+        self.address = address
+        self.mobile = mobile
+        self.email = email
+        self.homepage = homepage
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.address2 = address2
+        self.phone2 = phone2
+        self.notes = notes
